541677fec73c2e3b6ad883d322c986e418b4f71d
bandarupavankumar <bandarupavankumar555@gmail.com>
2026-01-20 11:26:03 +0530

    ok

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/common/header/Header";
@@ -7,7 +9,7 @@ import AuthInitializer from "@/components/pages/auth/AuthInitializer";
 
 export const metadata: Metadata = {
   title: "Babyshop | Online shopping places",
-  description: "Babyshop for onlne shopping",
+  description: "Babyshop for online shopping",
 };
 
 export default function RootLayout({
@@ -17,10 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
+      <body className="antialiased">
         <AuthInitializer />
         <Header />
-        <div className="bg-babyshopLightBg min-h-screen pb-20">{children}</div>
+        <div className="bg-babyshopLightBg min-h-screen pb-20">
+          {children}
+        </div>
         <Footer />
         <Toaster
           position="bottom-right"
